b26776944449ca91999c9de9ca925e20bf3b73e6
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2021-02-05 00:00:28 +0100

    update

--- ssh_laurie/phase_6.c
+++ ssh_laurie/phase_6.c
@@ -6,25 +6,39 @@ int	explode_bomb(void)
 	return (-1);
 }
 
-void phase_6(char *param_1)
+// in gdb -> info variables node1..6
+// p	node1 -> 253
+//		node2 -> 725
+//		node3 -> 301
+//		node4 -> 997
+//		node5 -> 212
+//		node6 -> 432
+
+void phase_6(void)
 {
 	int *piVar1;
 	int iVar2;
 	int *piVar3;
 	int iVar4;
-	undefined1 *local_38;
 	int *local_34[6];
+	int local_38[6];
 	int local_1c[6];
 
-	local_1c[0] = 1;
+	local_38[0] = 253;
+	local_38[1] = 725;
+	local_38[2] = 301;
+	local_38[3] = 997;
+	local_38[4] = 212;
+	local_38[5] = 432;
+	local_1c[0] = 5;
 	local_1c[1] = 1;
-	local_1c[2] = 1;
-	local_1c[3] = 1;
-	local_1c[4] = 1;
-	local_1c[5] = 1;
-	local_38 = node1;
+	local_1c[2] = 3;
+	local_1c[3] = 6;
+	local_1c[4] = 2;
+	local_1c[5] = 4;
 //	read_six_numbers(param_1,local_1c);
 	iVar4 = 0;
+	// check doublon
 	do {
 		if (5 < local_1c[iVar4] - 1U) {
 			explode_bomb();
@@ -46,14 +60,21 @@ void phase_6(char *param_1)
 		piVar3 = (int *)local_38;
 		if (1 < local_1c[iVar4]) {
 			do {
-				piVar3 = (int *)piVar3[2];
+				piVar3 = (int*)piVar3 + 2;
 				iVar2 = iVar2 + 1;
 			} while (iVar2 < local_1c[iVar4]);
 		}
 		local_34[iVar4] = piVar3;
 		iVar4 = iVar4 + 1;
 	} while (iVar4 < 6);
+	printf("ok\n");
 	iVar4 = 1;
+	int i = 0;
+	while (i < 6)
+	{
+		printf("%d\n", piVar3[i]);
+		i++;
+	}
 	piVar3 = local_34[0];
 	do {
 		piVar1 = local_34[iVar4];
@@ -64,11 +85,18 @@ void phase_6(char *param_1)
 	piVar1[2] = 0;
 	iVar4 = 0;
 	do {
-		if (*local_34[0] < *(int *)local_34[0][2]) {
+		if (*local_34[0] < *(int *)((local_34 + 0) + 2)) {
 			explode_bomb();
 		}
-		local_34[0] = (int *)local_34[0][2];
+		local_34[0] = (int *)((local_34 + 0) + 2);
 		iVar4 = iVar4 + 1;
 	} while (iVar4 < 5);
 	return;
+}
+
+
+int	main(void)
+{
+	phase_6();
+	return (0);
 }
